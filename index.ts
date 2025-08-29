2d296182e2539680648220924a3ece032d586b62
Arav menon <aravmenon.ak@gmail.com>
2025-08-29 09:39:11 +0530

    add new ops routes

--- index.ts
+++ index.ts
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   res.send("<h1>Backend is running perfectly</h1>");
 });
 
+app.get("/ops", (req, res) => {
+  res.send("<h1>Ops is updated now</h1>")
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
